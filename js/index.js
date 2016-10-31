e8284b9083a1f8d3185949ac6e0e6771fe145f70
yoursgk <KirillDmitrievich@MacBook-Air-Kirill.local>
2016-10-31 21:59:10 +0600

    +

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function windowSize() {
+    if ($(window).width() <= '768' && !($('*').is('.title-s'))) {
+        $(".navbar-header").append("<a class='title-s'>eBooks</a>");
+    }
+    if ($(window).width() >= '768') {
+        $("a.title-s").remove();
+    }
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if ($(window).width() >= '768') {
+    if (scrollTop >= 180) {
+        $('.navbar').addClass('fixed');
+        $('.title').css({'margin-bottom': '90px' });
+    } else {
+        $('.navbar').removeClass('fixed');
+        $('.title').css({'margin-bottom': '0' });
+    }}
+    else {
+        $('.navbar').addClass('fixed');
+        $('.title').css({'margin-bottom': '60px' });
+    }
+}
+$(window).load(windowSize);
+$(window).resize(windowSize);
+$(window).scroll(windowSize);
